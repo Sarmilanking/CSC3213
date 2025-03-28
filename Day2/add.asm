d9b4b77a4a2e6af18b896171aee559524cc25e2c
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2025-03-28 12:14:07 +0530

    Update add.asm

--- Day2/add.asm
+++ Day2/add.asm
@@ -1,1 +1,24 @@
+;write an assembly program to add two number and display the result.
+.model small
+.stack 100h
 
+.data
+n db 2
+m db 3
+
+.code 
+start:
+
+mov ax,@data
+mov ds,ax
+
+mov dl,n  ; dl=2
+add dl,m  ;dl=2+3=5 we convert asskey value
+add dl,48 ;dl=5+48=53
+
+mov ah, 02h
+int 21h
+
+mov ax,4c00h
+int 21h
+end start
