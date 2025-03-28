2002cf4c52a9656edd94c2fe99c6db008b8af68c
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2025-03-28 12:16:33 +0530

    Update mul.asm

--- Day2/mul.asm
+++ Day2/mul.asm
@@ -1,1 +1,26 @@
+;write an assembly program to sub two number and display the result.
+.model small
+.stack 100h
 
+.data
+n db 2
+m db 3
+
+.code
+start:
+
+mov ax,@data
+mov ds,ax
+
+mov al,n  	;al=2
+mul m		;al=2*3=6
+
+mov dl,al 	;dl=6
+add dl,48
+
+mov ah, 02h
+int 21h
+
+mov ax,4c00h
+int 21h
+end start
